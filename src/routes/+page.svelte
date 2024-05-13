6b15645b2809f995e1f7b8d7d7a2471c52e506ff
anandhuremanan <ananduor@gmail.com>
2024-05-13 16:07:11 +0530

    Added DatePicker Component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,8 @@
 <script lang="ts">
-	import Button from "$lib/button/Button.svelte";
-	import Modal from "$lib/modal/Modal.svelte";
-
-	let showModal = false;
+	import DatePicker from '$lib/datepicker/DatePicker.svelte';
+	let value: any;
 </script>
 
 <div>
-	<Button
-		on:click={() => {
-			showModal = !showModal;
-		}}>Submit</Button
-	>
-
-	<Modal bind:showModal autoclose modalTitle="Disclaimer" classModalContent="">
-		<p class="text-xs">
-			The information provided on this website is for general informational purposes only. It is not
-			intended to be a substitute for professional advice, diagnosis, or treatment. Always seek the
-			advice of your physician or other qualified health provider with any questions you may have
-			regarding a medical condition. Never disregard professional medical advice or delay in seeking
-			it because of something you have read on this website.
-		</p>
-		<div>
-			<Button class="mt-2">Accept</Button>
-		</div>
-	</Modal>
+	<DatePicker bind:value />
 </div>
